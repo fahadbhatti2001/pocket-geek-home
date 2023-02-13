2864c4bdb7946cbdd5502eb74acfefe4d35dfb23
Fahad Bhatti OSO <fahadqayyumbhatti2001@gmail.com>
2023-02-13 23:45:54 +0500

    Check 1

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 import { HashRouter } from "react-router-dom";
+import "tw-elements"
 import './Assets/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
