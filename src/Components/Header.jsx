ad94d13a3e42da3af5a2f856cc97425ec382ebb5
Fahad Bhatti OSO <fahadqayyumbhatti2001@gmail.com>
2023-02-14 00:17:32 +0500

    Check 2

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,23 +9,7 @@ export const Header = (props) => {
 
   return (
     <>
-      {/* <nav className="px-6 py-4 border-4 border-t-0 border-primary-3">
-          <div className="w-full flex justify-between items-center">
-              <img src="/images/pgh-logo.svg" className="" />
-              <div className="md:flex hidden items-center gap-4">
-                <div className="flex justify-center gap-12">
-                  {
-                    headerLinks.map((e, i) => (
-                      <Link key={i} to={e.path} className={"font-medium text-gray-500 " + e.cstStyle}>{e.name}</Link>
-                    ))
-                  }
-                </div>
-                <span className="h-8 w-px bg-gray-300"></span>
-                <img src="/images/drop.png" className="w-10" />
-              </div>
-          </div>
-      </nav> */}
-      <nav className="navbar navbar-expand-lg shadow-md bg-white relative flex items-center w-full justify-between px-6 py-4 border-4 border-t-0 border-primary-3">
+      <nav className="navbar navbar-expand-lg shadow-md bg-white relative flex items-center w-full justify-between px-6 py-4">
         <div className="px-6 w-full flex flex-wrap items-center justify-between">
           <img src="/images/pgh-logo.svg" className="" />
           <div className="flex items-center">
